a492c321048e38208d988d1466cf80c3a24aa2ea
kickymaulana <kickymaulana@gmail.com>
2024-05-16 13:34:40 +0700

    inisiaslisasi pertama kali

---
+++ app/src/main/java/com/kickymaulana/com/tandatangandigital/Login.java
@@ -0,0 +1,98 @@
+package com.kickymaulana.com.tandatangandigital;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.google.android.material.button.MaterialButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Objects;
+
+public class Login extends AppCompatActivity {
+
+    AppCompatEditText email;
+    AppCompatEditText password;
+    MaterialButton login;
+
+    RelativeLayout loading;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        email = (AppCompatEditText) findViewById(R.id.email);
+        password = (AppCompatEditText) findViewById(R.id.password);
+        login = (MaterialButton) findViewById(R.id.login);
+        loading = (RelativeLayout) findViewById(R.id.loading);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                loading.setVisibility(View.VISIBLE);
+                AndroidNetworking.post("https://1287-103-138-218-82.ngrok-free.app/tanda_tangan_digital_laravel/public/api/login")
+                        .addBodyParameter("email", Objects.requireNonNull(email.getText()).toString())
+                        .addBodyParameter("password", Objects.requireNonNull(password.getText()).toString())
+                        .setPriority(Priority.MEDIUM)
+                        .build()
+                        .getAsJSONObject(new JSONObjectRequestListener() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.d("RESPONSE", response.toString());
+                                try {
+                                    if (response.get("kode").equals("200")) {
+                                        Toast.makeText(Login.this, "kamu berhasil login", Toast.LENGTH_SHORT).show();
+                                        loading.setVisibility(View.GONE);
+                                    } else if (response.get("kode").equals("411")) {
+                                        String[] rule = {"email", "password"};
+                                        String pesanvalidasi = new PesanValidasi(rule, response).getpesan();
+                                        loading.setVisibility(View.GONE);
+                                        new AlertDialog.Builder(Login.this)
+                                                .setMessage(pesanvalidasi)
+                                                .show();
+
+
+                                    }
+                                } catch (JSONException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+
+                            @Override
+                            public void onError(ANError anError) {
+                                loading.setVisibility(View.GONE);
+                            }
+
+                        });
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/kickymaulana/com/tandatangandigital/PesanValidasi.java
@@ -0,0 +1,47 @@
+package com.kickymaulana.com.tandatangandigital;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class PesanValidasi {
+
+
+    String[] rule;
+    JSONObject response;
+
+    public PesanValidasi(String[] rule, JSONObject response) {
+        this.rule = rule;
+        this.response = response;
+        this.setpesan();
+    }
+
+    String pesanvalidasi = "";
+
+    public String getpesan(){
+        return this.pesanvalidasi;
+    }
+
+    public void setpesan(){
+
+        for(int i2 = 0; i2 < rule.length; i2++){
+            try {
+                if (response.getJSONObject("pesan").has(rule[i2])){
+                    int total = response.getJSONObject("pesan").getJSONArray(rule[i2]).length();
+                    for(int i = 0; i < total; i++){
+                        pesanvalidasi = pesanvalidasi + response.getJSONObject("pesan").getJSONArray(rule[i2]).get(i) + "\n";
+                    }
+
+                }
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+    }
+    public int hitung(){
+        return this.rule.length;
+    }
+
+}
