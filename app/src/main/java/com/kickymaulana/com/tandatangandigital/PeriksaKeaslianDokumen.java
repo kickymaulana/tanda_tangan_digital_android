9370d1df0278ea6d220d2ac344e422bc1b8c73f6
kickymaulana <kickymaulana@gmail.com>
2024-05-19 08:46:47 +0700

    berhasil membuat pilih dokumen dan pilih signature di cek keaslian

--- app/src/main/java/com/kickymaulana/com/tandatangandigital/PeriksaKeaslianDokumen.java
+++ app/src/main/java/com/kickymaulana/com/tandatangandigital/PeriksaKeaslianDokumen.java
@@ -1,17 +1,42 @@
 package com.kickymaulana.com.tandatangandigital;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.button.MaterialButton;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
 public class PeriksaKeaslianDokumen extends AppCompatActivity {
+
+    MaterialButton pilih_dokumen;
+    MaterialButton pilih_signature;
+
+    private ActivityResultLauncher<Intent> openFileLauncher;
+    private ActivityResultLauncher<Intent> openFileLauncher2;
+    Uri uri_hasil;
+    Uri uri_hasil2;
+    AppCompatTextView nama_dokumen;
+    AppCompatTextView nama_signature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +54,66 @@ public class PeriksaKeaslianDokumen extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle("Periksa Keaslian Dokumen");
+
+        pilih_dokumen = (MaterialButton) findViewById(R.id.pilih_dokumen);
+        pilih_signature = (MaterialButton) findViewById(R.id.pilih_signature);
+        nama_dokumen = (AppCompatTextView) findViewById(R.id.nama_dokumen);
+        nama_signature = (AppCompatTextView) findViewById(R.id.nama_signature);
+
+
+        openFileLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+
+                if (result.getResultCode() == RESULT_OK) {
+                    Intent data = result.getData();
+                    if (data != null) {
+                        Uri uri = data.getData();
+                        uri_hasil = uri;
+                        nama_dokumen.setText(getFileNameFromUri(uri_hasil));
+                    }
+                }
+
+            }
+        });
+
+        openFileLauncher2 = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+
+                if (result.getResultCode() == RESULT_OK) {
+                    Intent data = result.getData();
+                    if (data != null) {
+                        Uri uri = data.getData();
+                        uri_hasil2 = uri;
+                        nama_signature.setText(getFileNameFromUri(uri_hasil2));
+                    }
+                }
+
+            }
+        });
+        pilih_dokumen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("application/pdf");
+                openFileLauncher.launch(intent);
+
+            }
+        });
+
+        pilih_signature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("text/plain");
+                openFileLauncher2.launch(intent);
+
+            }
+        });
+
     }
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemid = item.getItemId();
@@ -36,5 +121,27 @@ public class PeriksaKeaslianDokumen extends AppCompatActivity {
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private String getFileNameFromUri(Uri uri) {
+        String fileName = null;
+        if (uri.getScheme().equals("content")) {
+            try (Cursor cursor = getContentResolver().query(uri, null, null, null, null)) {
+                if (cursor != null && cursor.moveToFirst()) {
+                    int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                    if (nameIndex >= 0) {
+                        fileName = cursor.getString(nameIndex);
+                    }
+                }
+            }
+        }
+        if (fileName == null) {
+            fileName = uri.getPath();
+            int cut = fileName.lastIndexOf('/');
+            if (cut != -1) {
+                fileName = fileName.substring(cut + 1);
+            }
+        }
+        return fileName;
     }
 }
