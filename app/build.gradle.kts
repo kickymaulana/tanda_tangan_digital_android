2e7c9b04640599cdfdb508a1592326ea1d2b1c54
kickymaulana <kickymaulana@gmail.com>
2024-05-28 10:38:09 +0700

    perbaikan kata kata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.kickymaulana.tandatangandigital"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
